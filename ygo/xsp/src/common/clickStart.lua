864eab37b25f34c13838076872989ec7ab6ce4b2
TerrenceFong <272786949@qq.com>
2019-08-05 08:15:19 +0800

    ygo fix clickstart

--- ygo/xsp/src/common/clickStart.lua
+++ ygo/xsp/src/common/clickStart.lua
@@ -4,7 +4,7 @@ local tab = require('common.tab')
 local function clickStart()
     sysLog('寻找开始点击按钮')
     mSleep(1500)
-    local x, y = findColors(
+    local x, y = findColor(
         {735, 530, 899, 613}, 
         "0|0|0xfec048,-1|28|0x845400,107|-6|0xffcb64,102|40|0xbe7600,40|17|0x413800,63|18|0xd2cf00,77|25|0x352100,51|-4|0xfec85c,52|31|0x895600",
         95, 0, 0, 0
